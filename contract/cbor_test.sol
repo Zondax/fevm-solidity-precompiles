70353c47511870f2459f827803adebaabad32305
Emmanuel <emmanuelm41@gmail.com>
2022-08-25 13:29:15 -0300

    add base code and ci to new contract

---
+++ contract/cbor_test.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <= 0.8.16;
+
+contract CborTest {
+
+    constructor() {}
+
+    function serialize() public returns(string result) {
+        return "";
+    }
+
+    function deserialize() public returns(bool valid) {
+        return true;
+    }
+
+}
