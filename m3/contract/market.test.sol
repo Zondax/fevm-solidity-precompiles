410830ea37c4c5d123d0601b0bc7deda7c21f7cf
Emmanuel <emmanuelm41@gmail.com>
2022-11-14 09:37:34 -0300

    get test cases for new functions on market actor

--- m3/contract/market.test.sol
+++ m3/contract/market.test.sol
@@ -16,4 +16,64 @@ contract FilecoinMarketMockTest {
 
         MarketAPI.add_balance(params);
     }
+
+    function get_balance_test() public{
+        CommonTypes.Addr memory params = CommonTypes.Addr(1, bytes("0x1111"));
+
+        MarketTypes.GetBalanceReturn memory response = MarketAPI.get_balance(params);
+    }
+
+    function get_deal_data_commitment_test() public{
+        MarketTypes.GetDealDataCommitmentParams memory params = MarketTypes.GetDealDataCommitmentParams(1);
+
+        MarketTypes.GetDealDataCommitmentReturn memory response = MarketAPI.get_deal_data_commitment(params);
+    }
+
+    function get_deal_client_test() public{
+        MarketTypes.GetDealClientParams memory params = MarketTypes.GetDealClientParams(1);
+
+        MarketTypes.GetDealClientReturn memory response = MarketAPI.get_deal_client(params);
+    }
+
+    function get_deal_provider_test() public{
+        MarketTypes.GetDealProviderParams memory params = MarketTypes.GetDealProviderParams(1);
+
+        MarketTypes.GetDealProviderReturn memory response = MarketAPI.get_deal_provider(params);
+    }
+
+    function get_deal_label_test() public{
+        MarketTypes.GetDealLabelParams memory params = MarketTypes.GetDealLabelParams(1);
+
+        MarketTypes.GetDealLabelReturn memory response = MarketAPI.get_deal_label(params);
+    }
+
+    function get_deal_term_test() public{
+        MarketTypes.GetDealTermParams memory params = MarketTypes.GetDealTermParams(1);
+
+        MarketTypes.GetDealTermReturn memory response = MarketAPI.get_deal_term(params);
+    }
+
+    function get_deal_epoch_price_test() public{
+        MarketTypes.GetDealEpochPriceParams memory params = MarketTypes.GetDealEpochPriceParams(1);
+
+        MarketTypes.GetDealEpochPriceReturn memory response = MarketAPI.get_deal_epoch_price(params);
+    }
+
+    function get_deal_client_collateral_test() public{
+        MarketTypes.GetDealClientCollateralParams memory params = MarketTypes.GetDealClientCollateralParams(1);
+
+        MarketTypes.GetDealClientCollateralReturn memory response = MarketAPI.get_deal_client_collateral(params);
+    }
+
+    function get_deal_provider_collateral_test() public{
+        MarketTypes.GetDealProviderCollateralParams memory params = MarketTypes.GetDealProviderCollateralParams(1);
+
+        MarketTypes.GetDealProviderCollateralReturn memory response = MarketAPI.get_deal_provider_collateral(params);
+    }
+
+    function get_deal_verified_test() public{
+        MarketTypes.GetDealVerifiedParams memory params = MarketTypes.GetDealVerifiedParams(1);
+
+        MarketTypes.GetDealVerifiedReturn memory response = MarketAPI.get_deal_verified(params);
+    }
 }
